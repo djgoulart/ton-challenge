1ca0652730e7b7a6daf818579281ac8992462d18
Diego Goulart <diegogoulart.aws@gmail.com>
2022-01-13 19:52:03 -0300

    refactor: ♻️ Routes code refactoring.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { usersRoutes } from "./routes/users.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/users", usersRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Node server running"));
